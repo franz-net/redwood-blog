a16259baf47f3a710d1de01cc57dfb7e2f9905d9
Franz <5334409+franz-net@users.noreply.github.com>
2022-08-26 14:58:52 -0500

    initial commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import { MetaTags } from "@redwoodjs/web";
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <p>This site was created to demonstrate my mastery of Redwood</p>
+    </>
+  );
+};
+
+export default AboutPage;
