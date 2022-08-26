f18fe528211c8a1d24426d0a33d9db35c1fc0db4
Franz <5334409+franz-net@users.noreply.github.com>
2022-08-26 15:57:36 -0500

    added auth

--- api/src/graphql/posts.sdl.js
+++ api/src/graphql/posts.sdl.js
@@ -7,8 +7,8 @@ export const schema = gql`
   }
 
   type Query {
-    posts: [Post!]! @requireAuth
-    post(id: Int!): Post @requireAuth
+    posts: [Post!]! @skipAuth
+    post(id: Int!): Post @skipAuth
   }
 
   input CreatePostInput {
@@ -26,4 +26,4 @@ export const schema = gql`
     updatePost(id: Int!, input: UpdatePostInput!): Post! @requireAuth
     deletePost(id: Int!): Post! @requireAuth
   }
-`
+`;
